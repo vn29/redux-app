3a000a5ce1a5cbdcf0e0f707263ae7da808d406a
vn29 <41770458+vn29@users.noreply.github.com>
2019-11-04 22:46:29 -0800

    now with working connection to the store

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+import {simpleAction} from './actions/simpleAction';
+import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+//this performs the simple action
+  simpleAction = (event) => {
+    this.props.simpleAction();
+  }
+  render() {
+    return (
+      <div className = "App">
+        <header className = "App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h1 className = "All-title">Wlecome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code> src/App.js</code> and save to reload
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <button onClick={this.simpleAction}>Test redux action</button>
+        <pre>
+          {
+            JSON.stringify(this.props)
+          }
+        </pre>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  ...state
+})
+
+const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+})
+
+//this is where the magic happens. I am connecting the state to props
+export default connect(mapStateToProps,mapDispatchToProps)(App);
