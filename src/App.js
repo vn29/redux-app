21fd92830f77950071986fea5e43b07d16d9610a
vn29 <41770458+vn29@users.noreply.github.com>
2019-11-05 19:50:48 -0800

    updates to the state

--- src/App.js
+++ src/App.js
@@ -5,11 +5,6 @@ import {simpleAction} from './actions/simpleAction';
 import logo from './logo.svg';
 
 class App extends Component {
-//this performs the simple action
-  SA = (event) => {
-    //call the dispatch mapped function
-    this.props.A();
-  }
   render() {
     return (
       <div className = "App">
@@ -20,7 +15,12 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code> src/App.js</code> and save to reload
         </p>
-        <button onClick={this.SA}>Test redux action</button>
+        <button onClick={
+          (e) => {this.props.A()}
+          }
+        >
+          Test redux action
+        </button>
         <pre>{JSON.stringify(this.props)}</pre>
       </div>
     );
@@ -29,8 +29,12 @@ class App extends Component {
 
 export default connect(
   //map state to props
+  // map state to props so it can be accessed by the component
+  //in other words, this is stuff coming into the component from the state that will render when actions are performed
   state => ({...state}),
   //map dispatch to props
+  //add A as a method under the props of the component
+  //in other words, this is stuff going out from the component to the state. where actions are dispatched
   dispatch => ({ 
     A() { 
       dispatch(simpleAction()) 
